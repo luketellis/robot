f7243270985b4a0add07fb0069df44b2a1b63761
Luke Tellis <luke.tellis@aroflo.com>
2021-10-26 22:45:49 +1100

    Added validateValidCoordinate to Grid

--- src/domain/Classes/Grid.js
+++ src/domain/Classes/Grid.js
@@ -1,8 +1,18 @@
-import { COLUMN_LENGTH, ROW_LENGTH } from '../config/constants';
+import { COLUMN_LENGTH, ERROR_MESSAGES, ROW_LENGTH } from '../config/constants';
 
 function Grid() {
 	this.rows = ROW_LENGTH;
 	this.columns = COLUMN_LENGTH;
 }
 
-export default Grid;
+const validateValidCoordinate = (coordinate) => {
+	if (coordinate.x < 0 || coordinate.y < 0) {
+		throw new Error(ERROR_MESSAGES.OUTSIDE_OF_GRID);
+	}
+
+	if (coordinate.x > ROW_LENGTH - 1 || coordinate.y > COLUMN_LENGTH - 1) {
+		throw new Error(ERROR_MESSAGES.OUTSIDE_OF_GRID);
+	}
+};
+
+export { Grid, validateValidCoordinate };
