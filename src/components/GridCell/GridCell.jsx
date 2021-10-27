0ac4633a54ae66510cae20782d5820ffe5d9fc7a
Luke Tellis <luke.tellis@aroflo.com>
2021-10-27 18:59:54 +1100

    Refactored Row to use robots from array

--- src/components/GridCell/GridCell.jsx
+++ src/components/GridCell/GridCell.jsx
@@ -3,10 +3,18 @@ import './GridCell.css';
 import { FaRobot } from 'react-icons/fa';
 
 export default function GridCell({ content, robot }) {
+	const turnDirectionIntoLowerCase = (direction) => {
+		return direction.toLowerCase();
+	};
+
 	return (
 		<span className="gridcell">
 			{content}
-			<span>{robot && <FaRobot className="" />}</span>
+			<span>
+				{robot && (
+					<FaRobot className={turnDirectionIntoLowerCase(robot.direction)} />
+				)}
+			</span>
 		</span>
 	);
 }
--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -2,37 +2,26 @@ import React from 'react';
 import './Row.css';
 import GridCell from '../GridCell/GridCell';
 import { COLUMN_LENGTH } from '../../domain/config/constants';
-import { Robot } from '../../domain/classes/Robot';
-import Coordinate from '../../domain/classes/Coordinate';
 
 export default function Row({ robotArray = [], rowNumber }) {
 	var cellsArray = [];
 
 	const doesGridCellAlreadyHaveRobot = (row, column) => {
-		let robotExists = false;
+		let robotFound = false;
 		robotArray.forEach((robot) => {
-			const currentCoordinate = new Coordinate(row, column);
 			if (robot.coordinate.x === row && robot.coordinate.y === column) {
-				robotExists = true;
+				robotFound = robot;
 			}
 		});
-		return robotExists;
+		return robotFound;
 	};
 
 	for (var columnNumber = 0; columnNumber < COLUMN_LENGTH; columnNumber++) {
-		// console.log(
-		// 	rowNumber,
-		// 	columnNumber,
-		// 	doesGridCellAlreadyHaveRobot(rowNumber, columnNumber)
-		// );
 		cellsArray.push(
 			<GridCell
 				key={columnNumber}
 				content={`${rowNumber}, ${columnNumber}`}
-				robot={
-					doesGridCellAlreadyHaveRobot(rowNumber, columnNumber) &&
-					new Robot(1, new Coordinate(rowNumber, columnNumber), 'NORTH', true)
-				}
+				robot={doesGridCellAlreadyHaveRobot(rowNumber, columnNumber)}
 			/>
 		);
 	}
